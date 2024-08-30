24a560b176dc7bf747a5758e5c8c873542a16701
Abdul-Qayyum Olatunji <106759130+Qayyax@users.noreply.github.com>
2024-08-29 20:16:20 -0400

    working on the footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import DarkModeProvider from "./components/DarkModeProvider"
+import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Home from "./components/home/Home"
 import Project from "./components/projects/Project"
@@ -12,6 +13,7 @@ function App() {
         <Header />
         <Home data={projectData} />
         <Project data={projectData} />
+        <Footer />
       </DarkModeProvider>
     </>
   )
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,40 @@
-export default function Footer() {
+import { useContext } from "react";
+import { DarkModeContext } from "./DarkModeProvider";
 
+export default function Footer() {
+  const { isDarkMode } = useContext(DarkModeContext)
+
+  return (
+    <footer>
+      <h2>
+        Let&apos;s work together! <span>Contact Me</span>
+      </h2>
+      <ul>
+        <li>
+          <span>EMAIL</span> <a href="mailto:qayyax@gmail.com">qayyax@gmail.com</a>
+        </li>
+        <li>
+          <span>LINKEDIN</span>
+          <a href="https://www.linkedin.com/in/tife-olatunji-b12729296"
+            target="_blank"
+          >
+            Tife Olatunji
+          </a>
+        </li>
+        <li>
+          <span>GITHUB</span>
+          <a
+            href="https://www.github.com/qayyax"
+            target="_blank"
+          >
+            @Qayyax</a>
+        </li>
+      </ul>
+
+      <div>
+        <p><span>&copy;</span> Tife Olatunji. All rights reserved</p>
+        <p>Designed by Marienoel Emechebe</p>
+      </div>
+    </footer>
+  )
 }
