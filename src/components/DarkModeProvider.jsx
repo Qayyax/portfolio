ee7453a293583fac6c84441fe7409b37944ffd4b
Abdul-Qayyum Olatunji <106759130+Qayyax@users.noreply.github.com>
2024-08-31 11:36:07 -0400

    dark mode

--- src/components/DarkModeProvider.jsx
+++ src/components/DarkModeProvider.jsx
@@ -2,14 +2,14 @@ import { createContext, useState } from "react";
 
 export const DarkModeContext = createContext()
 export default function DarkModeProvider({ children }) {
-  const [isDarkMode, setDarkMode] = useState(false)
+  const [isDarkMode, setDarkMode] = useState(true)
 
   function toggleDarkMode() {
     setDarkMode(prev => !prev)
   }
 
   return (
-    <DarkModeContext.Provider value={{isDarkMode, toggleDarkMode}}>
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
       {children}
     </DarkModeContext.Provider>
   )
