bf059397d450b27d3d0145ee861a89106c4981a8
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-04-06 00:14:03 -0400

    working on home text

--- app/ui/components/text/HomeText.tsx
+++ app/ui/components/text/HomeText.tsx
@@ -1,33 +1,36 @@
 import Link from "next/link";
+import { dotGothic16, doto } from "@/app/ui/fonts";
 
 export default function HomeText() {
+  const linkClasses = `text-purple-400 text-lg cursor-pointer ${doto.className} font-extrabold`;
   return (
     <section className="font-extrabold flex flex-col items-start justify-center gap-1 p-4">
-      <h1 className="text-xl ">Hi, I am Tife.</h1>
-      <p>
-        I am a fullstack developer with a master&apos;s degree in big data
-        analytics. Currently, I am working on improving my coding career. I can
-        build websites and apps. You can{" "}
-        <a
-          href="mailto:qayyax@gmail.com"
-          className="text-purple-400 cursor-pointer"
-        >
-          contact me
-        </a>
-        .
-      </p>
-      <p>
-        I love art, anime, tech, and games. Check out{" "}
-        <Link href="/my_space" className="text-purple-400 cursor-pointer">
-          {" "}
-          My_space{" "}
-        </Link>
-        for my interests and blog posts.
-      </p>
-      <p>
-        I have a store that is coming soon. Join my newsletter to stay informed
-        about it. I promise not to spam you with emails.
-      </p>
+      <h1 className="text-xl">Hi, I am Tife.</h1>
+      <div className={`${dotGothic16.className} tracking-wide`}>
+        <p>
+          I am a fullstack developer with a master&apos;s degree in big data
+          analytics. Currently, I am working on improving my coding career. I
+          can build websites and apps. You can{" "}
+          <a href="mailto:qayyax@gmail.com" className={linkClasses}>
+            {/* on hover the color should glow more or something*/}
+            contact me
+          </a>
+          .
+        </p>
+        <p>
+          I love art, anime, tech, and games. Check out{" "}
+          <Link href="/my_space" className={linkClasses}>
+            {/* on hover the color should glow more or something*/}
+            My_space{" "}
+          </Link>
+          for my interests and blog posts.
+        </p>
+        <p>
+          I have a store that is coming soon. Join my{" "}
+          <a className={linkClasses}>newsletter</a> to stay informed about it. I
+          promise not to spam you with emails.
+        </p>
+      </div>
     </section>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Doto, DotGothic16 } from "next/font/google";
+
+export const dotGothic16 = DotGothic16({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+export const doto = Doto({
+  subsets: ["latin"],
+});
