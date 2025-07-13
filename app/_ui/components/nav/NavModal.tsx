36ce36e04cf0cdfd0c93608095ff032f139fe837
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-07-13 15:38:56 -0400

    clicking left side closes modal

--- app/_ui/components/nav/NavModal.tsx
+++ app/_ui/components/nav/NavModal.tsx
@@ -6,13 +6,8 @@ import clsx from "clsx";
 import { MdClose } from "react-icons/md";
 import { NavProperty } from "@/type";
 
-// TODO:
-// - design of the navigations * (I guess I would come back to this)
-// - then move on to tab and desktop design
-
 const navRoutes: NavProperty[] = [
-  { title: "Home", to: "/" }, // would delete this later, or maybe I keep home
-  // I don't know if I need the home path
+  { title: "Home", to: "/" },
   { title: "About", to: "/about" },
   { title: "My_Space", to: "/my_space" },
   { title: "Projects", to: "/projects" },
@@ -24,36 +19,37 @@ type Props = {
 };
 
 export default function NavModal({ closeHamburgerAction }: Props) {
-  // I want it to have a glosry feel to it - I think?
-  // so black bg, purple accent color on the active nav *
-  // Modal, blury undertone, nav components for the mobile modal view
-  // would import the mobile Nav component  here to use all the routes
-  // Time to style the navigations
   const pathname = usePathname();
 
   return (
-    // another main div
-    // it has 2 w-1/2, left and right
-    // you click left, and the modal closes
     <>
-      <div className="h-screen absolute top-0 w-1/2 right-0 backdrop-blur-[3px] rounded-l-2xl z-20">
-        <div className="isolate w-full h-full bg-white/90 dark:bg-black/90 shadow-lg ring-1 ring-black/5 pt-20 relative rounded-l-2xl ">
-          <div onClick={closeHamburgerAction} className=" absolute top-4">
-            <MdClose className="text-4xl text-purple-500" />
-          </div>
+      <div>
+        {/* left side */}
+        {/* when you click this side we close hamburger */}
+        <div
+          onClick={closeHamburgerAction}
+          className="h-screen absolute top-0 w-1/2 left-0 backdrop-blur-[1px] "
+        ></div>
+        {/* right side */}
+        <div className="h-screen absolute top-0 w-1/2 right-0 backdrop-blur-[3px] rounded-l-2xl z-20">
+          <div className="isolate w-full h-full bg-white/90 dark:bg-black/90 shadow-lg ring-1 ring-black/5 pt-20 relative rounded-l-2xl ">
+            <div onClick={closeHamburgerAction} className=" absolute top-4">
+              <MdClose className="text-4xl text-purple-500" />
+            </div>
 
-          <ul className="flex flex-col gap-4 font-mono text-2xl items-center">
-            {navRoutes.map((nav, index) => (
-              <li
-                key={index}
-                className={clsx("some class here later", {
-                  "border-b-4 border-purple-500": pathname === nav.to, // border bottom purple here when active
-                })}
-              >
-                <MobileNav title={nav.title} to={nav.to} />
-              </li>
-            ))}
-          </ul>
+            <ul className="flex flex-col gap-4 font-mono text-2xl items-center">
+              {navRoutes.map((nav, index) => (
+                <li
+                  key={index}
+                  className={clsx("some class here later", {
+                    "border-b-4 border-purple-500": pathname === nav.to, // border bottom purple here when active
+                  })}
+                >
+                  <MobileNav title={nav.title} to={nav.to} />
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </>
