7fb7a025a1ab20de45046136b01c0f71a58dfe51
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-07-13 14:01:41 -0400

    made a nav-bar component

---
+++ app/_ui/layout/navbar-component.tsx
@@ -0,0 +1,18 @@
+import { doto } from "@/app/_ui/fonts";
+import DesktopNav from "./desktop-nav";
+import MobileLeft from "./mobile-nav-right";
+
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between items-center border-b-4 border-b-purple-500 p-3 sticky top-0 dark:bg-black bg-white">
+      <h3 className={`flex gap-2 font-extrabold text-2xl ${doto.className}`}>
+        Tife<span className="hidden md:block">Olatunji</span>
+      </h3>
+      <div>
+        {/* putting nav routes here for desktop and tab */}
+        <MobileLeft />
+        <DesktopNav />
+      </div>
+    </nav>
+  );
+}
