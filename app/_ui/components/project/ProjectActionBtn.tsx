e45f3502867db2deb92ac7999998f665249f55cb
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-07-17 14:03:28 -0400

    hover changed to black text in dark mode

--- app/_ui/components/project/ProjectActionBtn.tsx
+++ app/_ui/components/project/ProjectActionBtn.tsx
@@ -3,7 +3,7 @@ import { doto } from "../../fonts";
 import { ProjectAction } from "@/type";
 
 export default function ProjectActionBtn({ type, link }: ProjectAction) {
-  const style = `${doto.className} text-sm  border-2 border-purple-600 rounded-md px-2 hover:bg-purple-300 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-purple-300 active:dark:text-black`;
+  const style = `${doto.className} text-sm  border-2 border-purple-600 rounded-md px-2 hover:bg-purple-300 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-purple-300 active:dark:text-black hover:dark:text-black`;
   return (
     <>
       {type === "code" || type === "live-site" ? (
