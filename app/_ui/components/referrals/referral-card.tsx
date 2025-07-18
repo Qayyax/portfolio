6add0edef97d0cccd5c4ff0df63d02ee21bfda88
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-07-18 12:46:41 -0400

    border right and left for the referrals

--- app/_ui/components/referrals/referral-card.tsx
+++ app/_ui/components/referrals/referral-card.tsx
@@ -9,13 +9,15 @@ type Props = {
 
 export default function ReferralCard({ title, description, code }: Props) {
   return (
-    <div className="flex items-center justify-between gap-2 py-2 px-1 border-2 border-purple-400 rounded-xl">
+    <div className="flex items-center justify-between gap-2 py-2 px-1 border-2 border-purple-400 dark:border-purple-200 rounded-xl">
       {/* title */}
-      <p>{title}</p>
+      <p className="border-r-2 border-dashed pr-2">{title}</p>
       {/* description */}
       <p className="hidden md:block">{description}</p>
       {/* code */}
-      <ReferralBtn type={code.type} link={code.link} code={code.code} />
+      <div className="border-l-2 border-dashed pl-2">
+        <ReferralBtn type={code.type} link={code.link} code={code.code} />
+      </div>
     </div>
   );
 }
