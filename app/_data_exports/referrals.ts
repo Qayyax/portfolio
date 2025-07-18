42e4f4ba134070cf9ba57da82442078c1e78348a
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-07-18 11:54:19 -0400

    added referal links

---
+++ app/_data_exports/referrals.ts
@@ -0,0 +1,29 @@
+export const referrals = [
+  {
+    title: "Square One",
+    description: "For home and tenant insurance",
+    code: {
+      type: "link",
+      link: "https://www.squareone.ca/ref/PAVUQEVYXY",
+      code: "PAVUQEVYXY",
+    },
+  },
+  {
+    title: "Lemfi",
+    description: "For sending & receiving money overseas",
+    code: {
+      type: "code",
+      link: "",
+      code: "ABDUO3MP",
+    },
+  },
+  {
+    title: "BorderPass",
+    description: "Canadian immegration lawyers",
+    code: {
+      type: "link",
+      link: "app.borderpass.ai/refer/zRUmrb",
+      code: "zRUmrb",
+    },
+  },
+];
