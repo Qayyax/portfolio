5dd9556c10a9db5838f37f7e1981ad88ee3e25b1
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-07-18 12:51:56 -0400

    added style for referral plead

--- app/_ui/components/referrals/referral-cards.tsx
+++ app/_ui/components/referrals/referral-cards.tsx
@@ -1,8 +1,10 @@
 import { referrals } from "@/app/_data_exports/referrals";
 import ReferralCard from "./referral-card";
 import { ReferralBtnType } from "@/type";
+import { doto } from "../../fonts";
 
 export default function ReferralCards() {
+  const linkClasses = `text-purple-400 text-lg cursor-pointer ${doto.className} font-extrabold`;
   return (
     <div className="flex flex-col gap-3">
       {referrals.map((item) => (
@@ -14,9 +16,13 @@ export default function ReferralCards() {
         />
       ))}
 
-      <p className="mt-2">
+      <p className="mt-2 italic">
         Want me to join your referral program,{" "}
-        <a href="mailto:qayyax@gmail.com" target="_blank">
+        <a
+          href="mailto:qayyax@gmail.com"
+          target="_blank"
+          className={linkClasses}
+        >
           email me
         </a>
       </p>
