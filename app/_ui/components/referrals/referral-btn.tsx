736e96e13a1bbb094b0b65c311609463bb2efda1
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-07-18 11:13:48 -0400

    added referralCard component

--- app/_ui/components/referrals/referral-btn.tsx
+++ app/_ui/components/referrals/referral-btn.tsx
@@ -1,4 +1,18 @@
 type Props = {
   type: "code" | "link";
+  link?: string;
+  code?: string;
 };
-export default function ReferralBtn({ type }: Props) {}
+
+// TODO:
+// - [] tool tip
+// - [] prop for tool tip
+export default function ReferralBtn({ type, link, code }: Props) {
+  return type === "code" ? (
+    <button>{code}</button>
+  ) : (
+    <a href={link} target="_blank">
+      {code}
+    </a>
+  );
+}
