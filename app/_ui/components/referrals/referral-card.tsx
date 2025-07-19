bfb2d49956fd1738c7aa6e99f3811b01a7b11b63
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-07-18 20:13:21 -0400

    dotted line working now

--- app/_ui/components/referrals/referral-card.tsx
+++ app/_ui/components/referrals/referral-card.tsx
@@ -11,13 +11,14 @@ export default function ReferralCard({ title, description, code }: Props) {
   return (
     <div className="flex items-center justify-between gap-2 py-2 px-1 border-2 border-purple-400 dark:border-purple-200 rounded-xl">
       {/* title */}
-      <div className="border-r-2 border-dashed pr-2 ">
-        <p>{title}</p>
-      </div>
+      <p className="flex-1">{title}</p>
       {/* description */}
+      <div className="border-l-2 border-r-2 border-dashed pr-2 flex-1">
+        &nbsp;
+      </div>
       {/* <p className="hidden md:block">{description}</p> */}
       {/* code */}
-      <div className="border-l-2 border-dashed pl-2 ">
+      <div className=" flex-1 flex justify-end ">
         <ReferralBtn type={code.type} link={code.link} code={code.code} />
       </div>
     </div>
