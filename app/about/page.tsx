223f579f3e71c464618ba45b152e7eebfb511853
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-07-20 22:17:52 -0400

    starting about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,11 @@
+// TODO:
+// - [] About myself
+// - [] My journey
+// - [] My interest
+//    - [] show both youtube channels
+// - [] My current goals
+// - [] My dreams
+
 export default function About() {
   return <div>This is the about page</div>;
 }
