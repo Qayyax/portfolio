2ce7c0f80385acb394862b74909e223e4ec1c81c
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-07-21 11:47:03 -0400

    about section component

--- app/_ui/components/about/about-section.tsx
+++ app/_ui/components/about/about-section.tsx
@@ -1,1 +1,15 @@
-export default function AboutSection() {}
+import { doto } from "../../fonts";
+
+type Props = {
+  header: string;
+  children: React.ReactNode;
+};
+
+export default function AboutSection({ header, children }: Props) {
+  return (
+    <section className="font-mono flex flex-col items-center justify-center p-2 gap-2 border-2 border-purple-400 dark:border-purple-200 rounded-2xl">
+      <h3 className={`font-extrabold text-2xl ${doto.className}`}>{header}</h3>
+      {children}
+    </section>
+  );
+}
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,6 +5,7 @@
 //    - [] show both youtube channels
 // - [] My current goals
 // - [] My dreams
+// - [] under each sections would have a divider in the center
 
 export default function About() {
   return <div>This is the about page</div>;
