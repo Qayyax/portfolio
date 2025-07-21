912c5744c20992aafccb42b8555f6152aa6ce054
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-07-21 12:16:41 -0400

    removed about, added todo

--- app/_ui/components/my_space/space-sections.tsx
+++ app/_ui/components/my_space/space-sections.tsx
@@ -5,7 +5,7 @@ type Props = {
   children: React.ReactNode;
 };
 
-export default function AboutSection({ header, children }: Props) {
+export default function Section({ header, children }: Props) {
   return (
     <section className="font-mono flex flex-col items-center justify-center p-2 gap-2 border-2 border-purple-400 dark:border-purple-200 rounded-2xl">
       <h3 className={`font-extrabold text-2xl ${doto.className}`}>{header}</h3>
--- app/my_space/page.tsx
+++ app/my_space/page.tsx
@@ -1,3 +1,15 @@
+// TODO:
+// - [] iframe that comes up when I am streaming.
+// - [] multisections
+// - [] Apps I find interesting
+// - [] Interest gallery
+//    - [] Art gallery
+//    - [] Video galley of my favourite contents
+//        - [] break this into sections as well
+//        - [] my old cod videos,
+//        - [] videos I find funny
+//        - [] tech videos
+// - [] Blogs
 export default function MySpace() {
   return <div>This is My_Space page</div>;
 }
