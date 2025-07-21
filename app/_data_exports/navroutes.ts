72c1654a00f6c1f64d19673df3e8cff20d4e4519
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-07-21 12:04:58 -0400

    removed about section

--- app/_data_exports/navroutes.ts
+++ app/_data_exports/navroutes.ts
@@ -2,7 +2,6 @@ import { NavProperty } from "@/type";
 
 const navRoutes: NavProperty[] = [
   { title: "Home", to: "/" },
-  { title: "About", to: "/about" },
   { title: "My_Space", to: "/my_space" },
   { title: "Projects", to: "/projects" },
   { title: "Store", to: "/store" },
