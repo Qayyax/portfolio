6d6197ba26f75b7f6b21b6a9b2a19fd8221c5dd8
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-07-24 21:07:01 -0400

    mobile section for myspace

---
+++ app/_ui/layout/my_space/mobile-section.tsx
@@ -0,0 +1,29 @@
+import Iframe from "../../components/my_space/iframe";
+import Section from "../../components/my_space/space-sections";
+
+export default function MySpaceMobile() {
+  return (
+    <section className="p-2 flex flex-col gap-4">
+      {/* Now streaming pane */}
+      <div>
+        {" "}
+        <Iframe />
+      </div>
+      <Section header="Interesting Apps">
+        <div>component goes here</div>
+      </Section>
+
+      <Section header="Art Gallery">
+        <div>component goes here</div>
+      </Section>
+
+      <Section header="Video Gallery">
+        <div>component goes here</div>
+      </Section>
+
+      <Section header="Anime List">
+        <div>component goes here</div>
+      </Section>
+    </section>
+  );
+}
--- app/my_space/page.tsx
+++ app/my_space/page.tsx
@@ -1,6 +1,4 @@
 // TODO:
-// - [x] iframe that comes up when I am streaming.
-// - [x] multisections
 // - [] Apps I find interesting
 // - [] Interest gallery
 //    - [] Art gallery
@@ -9,40 +7,19 @@
 //        - [] my old cod videos,
 //        - [] videos I find funny
 //        - [] tech videos
-//
+
 // NOTE:
 // So I think for mobile, they would click the component
 // which would take them to the page
 // But on desktop, they would see snippet, then click to get the full thing
-// would move the sections below to mobile component
 
-import Iframe from "../_ui/components/my_space/iframe";
-import Section from "../_ui/components/my_space/space-sections";
+import MySpaceMobile from "../_ui/layout/my_space/mobile-section";
 
 // - [] Blogs
 export default function MySpace() {
   return (
-    <section className="p-2 flex flex-col gap-4">
-      {/* Now streaming pane */}
-      <div>
-        {" "}
-        <Iframe />
-      </div>
-      <Section header="Interesting Apps">
-        <div>component goes here</div>
-      </Section>
-
-      <Section header="Art Gallery">
-        <div>component goes here</div>
-      </Section>
-
-      <Section header="Video Gallery">
-        <div>component goes here</div>
-      </Section>
-
-      <Section header="Anime List">
-        <div>component goes here</div>
-      </Section>
-    </section>
+    <>
+      <MySpaceMobile />
+    </>
   );
 }
