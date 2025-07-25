832ce2a400ec4f210bf830e22738aac5614d0fd0
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-07-25 11:57:59 -0400

    apps componenet

---
+++ app/_ui/components/my_space/interesting-app.tsx
@@ -0,0 +1,9 @@
+// This is the component for interesting apps
+type Props = {
+  name: string;
+  platform: string;
+  description: string;
+};
+export default function InterestingApp({ name, platform, description }: Props) {
+  return <div></div>;
+}
