ae97ac44fe280427e27461518eba42f42dec201e
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-07-25 10:23:11 -0400

    removed todo

--- app/_ui/components/my_space/space-sections.tsx
+++ app/_ui/components/my_space/space-sections.tsx
@@ -5,9 +5,6 @@ type Props = {
   children: React.ReactNode;
 };
 
-// TODO:
-// - [] desktop vs mobile view (centering)
-
 export default function Section({ header, children }: Props) {
   return (
     <section className="font-mono flex flex-col items-center justify-center p-2 gap-2 border-2 border-purple-400 dark:border-purple-200 rounded-xl">
