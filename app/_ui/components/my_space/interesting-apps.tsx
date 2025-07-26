5f23b6fc3011c98be2e25d4a4883d762837ff575
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-07-26 19:47:18 -0400

    map function for interesting app component

---
+++ app/_ui/components/my_space/interesting-apps.tsx
@@ -0,0 +1,20 @@
+import { InterestingAppType } from "@/type";
+import InterestingApp from "./interesting-app";
+
+type Props = {
+  appData: InterestingAppType[];
+};
+export default function InterestingApps({ appData }: Props) {
+  return (
+    <div>
+      {appData.map((app) => (
+        <InterestingApp
+          name={app.name}
+          description={app.description}
+          platform={app.platform}
+          link={app.link}
+        />
+      ))}
+    </div>
+  );
+}
