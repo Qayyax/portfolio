c8edd46dc8e9ffed277bb6fec3d54e0a781482e8
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-07-26 01:12:49 -0400

    mdn docs saved me, thanks Fuad

--- app/_ui/components/referrals/referral-card.tsx
+++ app/_ui/components/referrals/referral-card.tsx
@@ -7,14 +7,17 @@ type Props = {
   code: ReferralBtnType;
 };
 
+// TODO:
+// on Hover : - animate the description under this
+
 export default function ReferralCard({ title, description, code }: Props) {
   return (
     <div className="relative group">
-      <div className="grid grid-cols-3 items-center justify-between gap-2 py-2 px-1 border-2 border-purple-400 dark:border-purple-200 rounded-xl divide-x-3 divide-dashed divide-black dark:divide-white">
+      <div className=" grid grid-cols-3 items-center justify-between gap-2 py-2 px-1 border-2 border-purple-400 dark:border-purple-200 rounded-xl divide-x-3 divide-dashed divide-black dark:divide-white">
         {/* title */}
         <p>{title}</p>
         {/* description */}
-        <div className="">&nbsp;</div>
+        <div>&nbsp;</div>
 
         {/* Referral code */}
         <div className=" flex justify-end ">
@@ -23,7 +26,7 @@ export default function ReferralCard({ title, description, code }: Props) {
       </div>
 
       {/* Tooltip */}
-      <div className="absolute left-1/2 transform -translate-x-1/2 bottom-full mb-2 hidden group-hover:block bg-gray-800 text-white text-xs rounded py-1 px-2 ">
+      <div className="absolute left-1/2 transform -translate-x-1/2 bottom-full mb-2 hidden group-hover:block bg-gray-800 text-white text-xs rounded py-1 px-2 z-10 ">
         {description}
       </div>
     </div>
