b75d0e1ee5bb1eb0c6737813c7b7b9241aa62a33
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-07-25 22:30:59 -0400

    small design

--- app/_ui/components/my_space/interesting-app.tsx
+++ app/_ui/components/my_space/interesting-app.tsx
@@ -5,5 +5,19 @@ type Props = {
   description: string;
 };
 export default function InterestingApp({ name, platform, description }: Props) {
-  return <div></div>;
+  const border = "border-2 border-purple-400 dark:border-purple-200 rounded-xl";
+  return (
+    <div className={`flex flex-col gap-2 p-2 ${border}`}>
+      {/* header div */}
+      <div className={`flex items-center justify-between font-extrabold `}>
+        <p>{name}</p>
+        <p>{platform}</p>
+      </div>
+
+      {/* description div */}
+      <div className={`${border} p-2 font-mono`}>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
 }
--- app/my_space/apps/page.tsx
+++ app/my_space/apps/page.tsx
@@ -1,3 +1,15 @@
+import InterestingApp from "@/app/_ui/components/my_space/interesting-app";
+
 export default function InterestingAppsPage() {
-  return <div>This is interesting apps page</div>;
+  return (
+    <div>
+      This is interesting apps page
+      {/* section for the apps */}
+      <InterestingApp
+        name="Loop"
+        description="Used to tile your apps on mac"
+        platform="MacOS"
+      />
+    </div>
+  );
 }
