fb102084bb0850a770b592763dd976fd6a25d0df
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-08-15 23:56:48 -0400

    rearange

--- app/_data_exports/referrals.ts
+++ app/_data_exports/referrals.ts
@@ -1,11 +1,11 @@
 export const referrals = [
   {
-    title: "Square One",
-    description: "For home and tenant insurance",
+    title: "SkillShare",
+    description: "One month free on SkillShare",
     code: {
       type: "link",
-      link: "https://www.squareone.ca/ref/PAVUQEVYXY",
-      code: "PAVUQEVYXY",
+      link: "https://skl.sh/4oDFtkn",
+      code: "4oDFtkn",
     },
   },
   {
@@ -18,21 +18,21 @@ export const referrals = [
     },
   },
   {
+    title: "Square One",
+    description: "For home and tenant insurance",
+    code: {
+      type: "link",
+      link: "https://www.squareone.ca/ref/PAVUQEVYXY",
+      code: "PAVUQEVYXY",
+    },
+  },
+  {
     title: "BorderPass",
     description: "Canadian immigration lawyers",
     code: {
       type: "link",
       link: "app.borderpass.ai/refer/zRUmrb",
       code: "zRUmrb",
-    },
-  },
-  {
-    title: "SkillShare",
-    description: "One month free on SkillShare",
-    code: {
-      type: "link",
-      link: "https://skl.sh/4oDFtkn",
-      code: "4oDFtkn",
     },
   },
 ];
