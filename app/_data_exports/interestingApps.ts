414f0259a2f9b9d3c6f85c8e3be7d8b1409a6cf3
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-08-30 07:52:41 -0400

    array for interesting apps

---
+++ app/_data_exports/interestingApps.ts
@@ -0,0 +1,11 @@
+import { InterestingAppType } from "@/type";
+
+// interesting apps go here
+const interestingApps: InterestingAppType[] = [
+  {
+    name: "Loop",
+    description: "Used to tile your apps on mac",
+    platform: "MacOS",
+    link: "https://github.com/MrKai77/Loop",
+  },
+];
