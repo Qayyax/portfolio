730c417908b9406b5ce534e81a9bd0da848c1c67
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-09-20 12:26:22 -0400

    added backdrop

--- app/_ui/layout/navbar.tsx
+++ app/_ui/layout/navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center border-b-4 border-b-purple-500 p-3 sticky top-0   bg-white/90 dark:bg-black/90 z-90">
+    <nav className="flex justify-between items-center border-b-4 border-b-purple-500 p-3 sticky top-0   bg-white/90 dark:bg-black/90 z-90 backdrop-blur-sm">
       <h3 className={` font-extrabold text-2xl ${doto.className}`}>
         <Link href="/" className="flex gap-2">
           Tife
