798e030dfa21e6079ec2fa893c09cc4d549fd446
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-09-29 20:40:16 -0400

    commented out IG, not on the app

--- app/_ui/layout/footer.tsx
+++ app/_ui/layout/footer.tsx
@@ -4,7 +4,7 @@ import { FaYoutube as Youtube } from "react-icons/fa6";
 import { FaGithub as Github } from "react-icons/fa";
 import { FaLinkedin as LinkedIn } from "react-icons/fa6";
 import { FaXTwitter as Twitter } from "react-icons/fa6";
-import { FaInstagram as Instagram } from "react-icons/fa6";
+// import { FaInstagram as Instagram } from "react-icons/fa6";
 import { IoMailOutline as Gmail } from "react-icons/io5";
 import { SiBuymeacoffee as Coffee } from "react-icons/si";
 import { FaPalette as Palette } from "react-icons/fa6";
@@ -31,12 +31,12 @@ const links = [
     description: "Send me a message here :)",
     icon: LinkedIn,
   },
-  {
-    title: "Instagram",
-    link: "https://www.instagram.com/tife_len/",
-    icon: Instagram,
-    description: "I post art and random things, chat with me here",
-  },
+  // {
+  //   title: "Instagram",
+  //   link: "https://www.instagram.com/tife_len/",
+  //   icon: Instagram,
+  //   description: "I post art and random things, chat with me here",
+  // },
   {
     title: "Art Gallery",
     link: "https://www.deviantart.com/qayyax/gallery",
